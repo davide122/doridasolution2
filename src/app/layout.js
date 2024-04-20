413aa05ad03b22ea5a50380ae087e50110c31d42
davide122 <davidemarchica277@gmail.com>
2024-04-20 18:54:52 +0200

    modifica di acluni file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css'; // Aggiungi questa linea
 import "./globals.css"
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="it">
+       <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>{metadata.title}</title>
+  <meta name="description" content={metadata.description} />
+  <link rel="canonical" href={`https://${metadata.canonical}`} />
+  <meta property="og:type" content={metadata.openGraph.type} />
+  <meta property="og:locale" content={metadata.openGraph.locale} />
+  <meta property="og:url" content={`https://${metadata.openGraph.url}`} />
+  <meta property="og:title" content={metadata.openGraph.title} />
+  <meta property="og:description" content={metadata.openGraph.description} />
+  <meta property="og:image" content={metadata.openGraph.image} />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );

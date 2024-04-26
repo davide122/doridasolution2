0bb422fb979b9d81643972c83e46ade7e7856020
davide122 <davidemarchica277@gmail.com>
2024-04-26 21:12:18 +0200

    termscondiction and dashboard

--- src/components/Legal/TermsCondiction.jsx
+++ src/components/Legal/TermsCondiction.jsx
@@ -58,14 +58,14 @@ const TermCondiction = () => {
         <p>
           Benvenuti a Dorida Solution e DoridaSolution Music, offerti da Davide
           Marchica, Dorin Ciofalo e Rino Ciofalo. Questi Termini e Condizioni
-          regolano l'accesso e l'utilizzo dei nostri servizi disponibili
+          regolano l&apos;accesso e l&apos;utilizzo dei nostri servizi disponibili
           attraverso il sito web www.doridasolution.com.
         </p>
       </section>
       <section id="serviceDescription">
         <h2>2. Accettazione dei Termini</h2>
         <p>
-          Utilizzando i nostri servizi, l'utente accetta di essere vincolato da
+          Utilizzando i nostri servizi, l&apos;utente accetta di essere vincolato da
           questi Termini e Condizioni, comprese tutte le politiche aggiuntive e
           le future modifiche che possono essere pubblicate sul sito o
           comunicate agli utenti.
@@ -78,16 +78,16 @@ const TermCondiction = () => {
           DoridaSolution Music permette agli utenti di caricare e condividere
           contenuti musicali. Gli utenti sono responsabili per il rispetto dei
           diritti di proprietà intellettuale e delle leggi relative ai diritti
-          d'autore.
+          d&apos;autore.
         </p>
       </section>
       <section id="privacy">
         <h2>4. Proprietà Intellettuale</h2>
         <p>
           Il codice, il design e gli elementi grafici di Dorida Solution e
-          DoridaSolution Music sono protetti dalle leggi sul diritto d'autore e
+          DoridaSolution Music sono protetti dalle leggi sul diritto d&apos;autore e
           altre leggi sulla proprietà intellettuale. Davide Marchica detiene i
-          diritti d'autore sul codice originale del sito web. Tutti i diritti
+          diritti d&apos;autore sul codice originale del sito web. Tutti i diritti
           non espressamente concessi in questi termini sono riservati. Gli
           utenti possono caricare contenuti musicali di cui detengono i diritti
           o per i quali hanno ottenuto il permesso necessario. Tali contenuti
@@ -95,7 +95,7 @@ const TermCondiction = () => {
           concedono a Dorida Solution una licenza non esclusiva, trasferibile,
           sottolicenziabile, senza royalties, valida a livello globale, per
           utilizzare, riprodurre, distribuire, preparare opere derivate,
-          visualizzare ed eseguire tali contenuti in relazione all'operato di
+          visualizzare ed eseguire tali contenuti in relazione all&apos;operato di
           Dorida Solution e DoridaSolution Music.
         </p>
       </section>
@@ -103,7 +103,7 @@ const TermCondiction = () => {
         <h2>5. Privacy e Protezione dei Dati Personali</h2>
         <p>
           In conformità con il Regolamento Generale sulla Protezione dei Dati
-          (GDPR) dell'Unione Europea, la nostra Politica sulla Privacy descrive
+          (GDPR) dell&apos;Unione Europea, la nostra Politica sulla Privacy descrive
           come raccogliamo, conserviamo e proteggiamo le informazioni personali
           degli utenti.
         </p>
@@ -112,7 +112,7 @@ const TermCondiction = () => {
         <h2>6. Utilizzo del Servizio</h2>
         <p>
           Gli utenti si impegnano a non utilizzare i servizi per scopi illegali
-          o per violare i diritti di terze parti. L'utente accetta di non
+          o per violare i diritti di terze parti. L&apos;utente accetta di non
           distribuire virus o altri codici malevoli e di non tentare di accedere
           in modo non autorizzato al sistema o ai dati altrui.
         </p>
@@ -121,8 +121,8 @@ const TermCondiction = () => {
         <h2>7. Limitazione di Responsabilità</h2>
         <p>
           Dorida Solution non sarà responsabile per danni diretti o indiretti
-          derivanti dall'uso dei servizi, inclusi, ma non limitati a, perdite
-          economiche, perdita di dati o interruzioni dell'attività. Questa
+          derivanti dall&apos;uso dei servizi, inclusi, ma non limitati a, perdite
+          economiche, perdita di dati o interruzioni dell&apos;attività. Questa
           limitazione di responsabilità si applica nella misura massima
           consentita dalla legge.
         </p>
@@ -131,8 +131,8 @@ const TermCondiction = () => {
         <h2>8. Modifiche ai Termini</h2>
         <p>
           Dorida Solution si riserva il diritto di modificare questi termini in
-          qualsiasi momento. È responsabilità dell'utente rivedere regolarmente
-          i termini per eventuali modifiche. L'uso continuato dei servizi dopo
+          qualsiasi momento. È responsabilità dell&apos;utente rivedere regolarmente
+          i termini per eventuali modifiche. L&apos;uso continuato dei servizi dopo
           tali modifiche costituirà accettazione dei nuovi termini.
         </p>
       </section>
